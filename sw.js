f748e59496ab785d0d3991adc793bec38dfefe1e
Takisplans <geronths@yahoo.gr>
2026-07-12 23:29:58 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump CACHE_NAME on every release so old caches get cleared and clients pick up new files.
-const CACHE_NAME = 'takisplan-cache-v10';
+const CACHE_NAME = 'takisplan-cache-v11';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
